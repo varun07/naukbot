8fa8af4fc761e3150466417d4d0fd8067556d6cf
Varun Singh <varun.singh@naukri.com>
2018-07-01 02:09:24 +0530

    set up express js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var allWalkins = require('../walkins');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  allWalkins.filter((walkin) => walkin.id.toString() === req.params['id']);
+  res.render('index', 
+    { 
+      title: 'Express',
+      walkin: (allWalkins.length || {}) && allWalkins[0]
+    });
 });
 
 module.exports = router;
+
+
